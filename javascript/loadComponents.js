323692bf3cf30c2cdc371877a0bef494cd6fde4a
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-10-31 23:23:09 -0300

    fix: loadComponents.js

--- javascript/loadComponents.js
+++ javascript/loadComponents.js
@@ -1,38 +1,42 @@
-// Cache para armazenar conteúdo dos componentes já carregados
-const componentCache = {};
-
+// Exemplo de loadComponents.js
 async function loadComponent(id, path) {
-  // Verifica a existencia na cache
-  if (componentCache[path]) {
-    document.getElementById(id).innerHTML = componentCache[path];
-    return;
-  }
-
   try {
     const response = await fetch(path);
     const content = await response.text();
     document.getElementById(id).innerHTML = content;
 
-    // Armazenar em cache
+    // Após carregar o header, chama a função para gerenciar a navegação
     setupNavigation();
-    componentCache[path] = content;
+    initializeDropdown();
   } catch (error) {
     console.error(`Failed to load component from ${path}`, error);
   }
 }
 
-// Lista de componentes para o carregamento dinamico
-const components = [
-  { id: "Header", path: "components/Header/Header.html" },
-  { id: "Banner", path: "components/Banner/Banner.html" },
-  { id: "Footer", path: "components/Footer/Footer.html" },
-];
+// Carregar o header e o banner
+Promise.all([
+  loadComponent("Header", "components/Header/Header.html"),
+  loadComponent("Banner", "components/Banner/Banner.html"),
+  loadComponent("Footer", "components/Footer/Footer.html"),
+]).then(() => {
+  // Apenas inicializa o dropdown após ambos os componentes serem carregados
+  initializeDropdown();
+});
 
-// Loop que carrega automatico os componentes
-components.forEach((component) => loadComponent(component.id, component.path));
+export function initializeDropdown() {
+  // Seleciona todos os links dropdown
+  const dropdownLinks = document.querySelectorAll(".dropdown-link");
+  dropdownLinks.forEach(link => {
+      link.addEventListener('click', () => {
+          // Seleciona o menu dropdown correspondente
+          const dropdownMenu = link.nextElementSibling; // Presumindo que o menu esteja imediatamente após o link
+          dropdownMenu.classList.toggle("show");
+      });
+  });
+}
+ 
 
-// Função para gerenciar a navegação
-function setupNavigation() {
+export function setupNavigation() {
   const navLinks = document.querySelectorAll("#nav-menu a");
   const sections = document.querySelectorAll("section");
 
@@ -46,7 +50,8 @@ function setupNavigation() {
       if (scrollPos >= sectionTop && scrollPos < sectionTop + sectionHeight) {
         navLinks.forEach(link => {
           link.classList.remove("active");
-          if (link.getAttribute("href").substring(1) === section.id) {
+          const hrefValue = link.getAttribute("href");
+          if (hrefValue && hrefValue.substring(1) === section.id) {
             link.classList.add("active");
           }
         });
@@ -61,15 +66,27 @@ function setupNavigation() {
   navLinks.forEach(link => {
     link.addEventListener("click", function (e) {
       e.preventDefault(); // Previne o comportamento padrão
-      const targetId = this.getAttribute("href").substring(1);
-      const targetSection = document.getElementById(targetId);
-
-      // Rolagem suave até a seção
-      targetSection.scrollIntoView({ behavior: "smooth" });
-
-      // Atualiza a classe active
-      navLinks.forEach(link => link.classList.remove("active"));
-      this.classList.add("active");
+      const hrefValue = this.getAttribute("href");
+  
+      if (hrefValue) { // Verifica se hrefValue não é null
+        const targetId = hrefValue.substring(1);
+        const targetSection = document.getElementById(targetId);
+  
+        // Verifica se a seção alvo existe antes de rolar
+        if (targetSection) {
+          // Rolagem suave até a seção
+          targetSection.scrollIntoView({ behavior: "smooth" });
+  
+          // Atualiza a classe active
+          navLinks.forEach(link => link.classList.remove("active"));
+          this.classList.add("active");
+        } else {
+          console.warn(`A seção com ID "${targetId}" não foi encontrada.`);
+        }
+      } else {
+        console.warn('Atributo href não encontrado no link.');
+      }
     });
   });
 }
+
